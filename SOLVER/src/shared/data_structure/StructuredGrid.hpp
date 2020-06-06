b3852accb1a5ab3d6b378a9b3919fb24a7ecbffd
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2020-06-06 05:11:37 +0100

    ..

--- SOLVER/src/shared/data_structure/StructuredGrid.hpp
+++ SOLVER/src/shared/data_structure/StructuredGrid.hpp
@@ -25,12 +25,14 @@ template <int D, typename T>
 class StructuredGrid {
 public:
     // constructor
-    StructuredGrid(const std::string &ncfile,
+    StructuredGrid() = delete;
+    
+    // constructor
+    StructuredGrid(const std::string &ncFile,
                    const std::array<std::string, D> &coordVarNames,
-                   const std::vector<std::pair<std::string, double>> &varInfo,
-                   const std::array<int, D> &shuffleData):
-    mNV((int)varInfo.size()) {
-        // verify shuffle data
+                   const std::vector<std::pair<std::string, double>> &dataInfo,
+                   const std::array<int, D> &shuffleData) {
+        // verify shuffle of data
         std::array<int, D> shuffleSorted = shuffleData;
         std::sort(shuffleSorted.begin(), shuffleSorted.end());
         for (int idim = 0; idim < D; idim++) {
@@ -40,13 +42,26 @@ public:
             }
         }
         
+        // find unique data variable names
+        std::set<std::string> uniqueDataVarNames;
+        for (auto it = dataInfo.begin(); it != dataInfo.end(); ++it) {
+            uniqueDataVarNames.insert(it->first);
+        }
+        
+        // data rank and factor
+        for (auto it = dataInfo.begin(); it != dataInfo.end(); ++it) {
+            int loc = (int)std::distance(uniqueDataVarNames.begin(),
+                                         uniqueDataVarNames.find(it->first));
+            mDataRankFactor.push_back({loc, it->second});
+        }
+        
         // read data
         timer::gPreloopTimer.begin("Reading grid data");
-        timer::gPreloopTimer.message("data file: " + io::popInputDir(ncfile));
+        timer::gPreloopTimer.message("data file: " + io::popInputDir(ncFile));
         std::vector<std::vector<double>> gridCoords;
         if (mpi::root()) {
             // open
-            NetCDF_Reader reader(io::popInputDir(ncfile));
+            NetCDF_Reader reader(io::popInputDir(ncFile));
             
             // read coords
             for (const std::string &cvName: coordVarNames) {
@@ -63,7 +78,7 @@ public:
             
             // allocate data
             Eigen::array<Eigen::DenseIndex, 1 + D> dimsData;
-            dimsData[0] = mNV; // # variables comes first
+            dimsData[0] = uniqueDataVarNames.size(); // # data rank comes first
             std::copy(dimsCrd.begin(), dimsCrd.end(), dimsData.begin() + 1);
             mGridData = Eigen::Tensor<T, 1 + D, Eigen::RowMajor>(dimsData);
             
@@ -74,19 +89,19 @@ public:
             count[0] = 1;
             
             // read data
-            for (int ivar = 0; ivar < mNV; ivar++) {
+            for (const std::string &vname: uniqueDataVarNames) {
                 // read to double
                 Eigen::Tensor<double, D, Eigen::RowMajor> dGridData;
-                reader.readTensorDouble(varInfo[ivar].first, dGridData);
-                dGridData = dGridData * varInfo[ivar].second;
+                reader.readTensorDouble(vname, dGridData);
                 // round
                 if (std::is_integral<T>::value) {
                     dGridData = dGridData.round();
                 }
                 // copy data
-                start[0] = ivar;
                 mGridData.slice(start, count).reshape(dimsCrd)
                 = dGridData.shuffle(shuffleData).template cast<T>();
+                // next
+                start[0]++;
             }
             
             // check
@@ -97,7 +112,7 @@ public:
                     ("StructuredGrid::StructuredGrid || "
                      "Incompatible grid sizes in coordinates and data. || "
                      "Dimension: " + coordVarNames[idim] + " || "
-                     "NetCDF file: " + ncfile);
+                     "NetCDF file: " + ncFile);
                 }
                 
                 // size
@@ -106,7 +121,7 @@ public:
                     ("StructuredGrid::StructuredGrid || "
                      "Too few grid points; at least two are required. || "
                      "Dimension: " + coordVarNames[idim] + " || "
-                     "NetCDF file: " + ncfile);
+                     "NetCDF file: " + ncFile);
                 }
                 
                 // sorted
@@ -115,7 +130,7 @@ public:
                     ("StructuredGrid::StructuredGrid || "
                      "Coordinates are not ascendingly sorted. || "
                      "Dimension: " + coordVarNames[idim] + " || "
-                     "NetCDF file: " + ncfile);
+                     "NetCDF file: " + ncFile);
                 }
             }
         }
@@ -132,7 +147,7 @@ public:
         // below we broadcast the tensor manually
         // add mpi::bcastEigenTensor if needed in the future
         Eigen::array<Eigen::DenseIndex, 1 + D> dims;
-        dims[0] = mNV;
+        dims[0] = uniqueDataVarNames.size();
         for (int idim = 0; idim < D; idim++) {
             dims[idim + 1] = mGridCoords[idim].size();
         }
@@ -145,20 +160,20 @@ public:
         
         // memory info
         timer::gPreloopTimer.message
-        (eigen_tools::memoryInfoTensor(mGridData, "structured_nu"));
+        (eigen_tools::memoryInfoTensor(mGridData, "unique grid data"));
         timer::gPreloopTimer.ended("Broadcasting grid data");
     }
     
     // in scope
-    template <typename csArray>
-    bool inScope(const csArray &targetCoords) const {
+    template <typename ArrayCS>
+    bool inScope(const ArrayCS &crdTarget) const {
         // linear interp 0-1 points
         int index0 = -1, index1 = -1;
         double factor0 = 0., factor1 = 0.;
         for (int idim = 0; idim < D; idim++) {
             try {
                 vector_tools::
-                linearInterpSorted(mGridCoords[idim], targetCoords[idim],
+                linearInterpSorted(mGridCoords[idim], crdTarget[idim],
                                    index0, index1, factor0, factor1);
             } catch (...) {
                 // location out of range
@@ -170,34 +185,34 @@ public:
     
     // compute
     typedef Eigen::Matrix<T, 1, Eigen::Dynamic> TRowV;
-    template <typename csArray>
-    TRowV compute(const csArray &targetCoords,
-                  const TRowV &valueForOutOfRange) const {
+    template <typename ArrayCS>
+    TRowV compute(const ArrayCS &crdTarget,
+                  const TRowV &valOutOfRange) const {
         // linear interp 0-1 points
         static std::array<std::array<int, D>, 2> index01;
         static std::array<std::array<double, D>, 2> factor01;
         for (int idim = 0; idim < D; idim++) {
             try {
                 vector_tools::
-                linearInterpSorted(mGridCoords[idim], targetCoords[idim],
+                linearInterpSorted(mGridCoords[idim], crdTarget[idim],
                                    index01[0][idim], index01[1][idim],
                                    factor01[0][idim], factor01[1][idim]);
             } catch (...) {
                 // location out of range
-                return valueForOutOfRange;
+                return valOutOfRange;
             }
         }
         
         // shapes
-        static Eigen::array<Eigen::DenseIndex, 1> shapeV = {mNV};
+        static Eigen::array<Eigen::DenseIndex, 1> shapeV = {numUniqueData()};
         static Eigen::array<Eigen::DenseIndex, 1 + D> start, count;
         start.fill(0);
         count.fill(1);
-        count[0] = mNV;
+        count[0] = numUniqueData();
         
         // loop over 2^D points
         typedef Eigen::Matrix<double, 1, Eigen::Dynamic> DRowV;
-        DRowV result = DRowV::Zero(mNV);
+        DRowV uData = DRowV::Zero(numUniqueData());
         for (int deci = 0; deci < pow(2, D); deci++) {
             int left = deci;
             double factorCombine = 1.;
@@ -209,38 +224,82 @@ public:
             const Eigen::Tensor<double, 1, Eigen::RowMajor> &row =
             factorCombine * mGridData.slice(start, count).reshape(shapeV)
             .template cast<double>();
-            result += Eigen::Map<const DRowV>(row.data(), mNV);
+            uData += Eigen::Map<const DRowV>(row.data(), numUniqueData());
+        }
+        
+        // map to non-unique and apply factor
+        DRowV data = DRowV::Zero(mDataRankFactor.size());
+        for (int ivar = 0; ivar < numData(); ivar++) {
+            int index = mDataRankFactor[ivar].first;
+            double factor = mDataRankFactor[ivar].second;
+            data(ivar) = uData(index) * factor;
         }
         
         // round
         if (std::is_integral<T>::value) {
-            result = result.array().round();
+            data = data.array().round();
         }
-        return result.template cast<T>();
+        return data.template cast<T>();
     }
     
     // compute single
-    template <typename csArray>
-    T compute(const csArray &targetCoords, T valueForOutOfRange,
+    template <typename ArrayCS>
+    T compute(const ArrayCS &crdTarget, T valOutOfRange,
               int varIndex = 0) const {
-        TRowV outVal = TRowV::Zero(mNV);
-        outVal(varIndex) = valueForOutOfRange;
-        return compute(targetCoords, outVal)(varIndex);
+        TRowV oneVal = TRowV::Zero(numData());
+        oneVal(varIndex) = valOutOfRange;
+        return compute(crdTarget, oneVal)(varIndex);
     }
     
-    // get
+    // get grid coords
     const std::array<std::vector<double>, D> &getGridCoords() const {
         return mGridCoords;
     }
     
-    // get
+    // get grid coords
     std::array<std::vector<double>, D> &getGridCoords() {
         return mGridCoords;
     }
     
-    // get
+    // get grid data
     const Eigen::Tensor<T, 1 + D, Eigen::RowMajor> &getGridData() const {
         return mGridData;
+    }
+    
+    // get data range
+    eigen::DMatXX getDataRange() const {
+        // min/max of unique data
+        eigen::DMatXX uniqueMinMax(numUniqueData(), 2);
+        Eigen::array<Eigen::DenseIndex, 1 + D> start, count;
+        start.fill(0);
+        count = mGridData.dimensions();
+        count[0] = 1;
+        typedef Eigen::Tensor<double, 0, Eigen::RowMajor> Tensor0;
+        for (; start[0] < numUniqueData(); start[0]++) {
+            const auto &sliced = mGridData.slice(start, count);
+            uniqueMinMax(start[0], 0) = ((Tensor0)sliced.minimum())(0);
+            uniqueMinMax(start[0], 1) = ((Tensor0)sliced.maximum())(0);
+        }
+        
+        // min/max of data
+        eigen::DMatXX dataMinMax(numData(), 2);
+        for (int ivar = 0; ivar < numData(); ivar++) {
+            int index = mDataRankFactor[ivar].first;
+            double factor = mDataRankFactor[ivar].second;
+            dataMinMax(ivar, 0) = uniqueMinMax(index, 0) * factor;
+            dataMinMax(ivar, 1) = uniqueMinMax(index, 1) * factor;
+        }
+        return dataMinMax;
+    }
+    
+    // get number of data
+    inline const int numData() const {
+        return (int)mDataRankFactor.size();
+    }
+    
+    // get number of unique data
+    inline const int numUniqueData() const {
+        return (int)mGridData.dimension(0);
     }
     
 private:
@@ -248,8 +307,10 @@ private:
     std::array<std::vector<double>, D> mGridCoords;
     // grid data
     Eigen::Tensor<T, 1 + D, Eigen::RowMajor> mGridData;
-    // number of variables
-    const int mNV;
+    // data rank and factor
+    // NOTE: to avoid store duplicated data using the same nc variable
+    //       but different factors
+    std::vector<std::pair<int, double>> mDataRankFactor;
 };
 
 #endif /* StructuredGrid_hpp */
